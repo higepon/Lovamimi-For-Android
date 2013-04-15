2b35496b9cb614b8e1fe8ea0bb2277506ed451e0
Taro Minowa Higepon <higepon@gmail.com>
2013-04-14 21:05:45 -0700

    Print secret body.

--- src/com/lovamimi/MainActivity.java
+++ src/com/lovamimi/MainActivity.java
@@ -1,30 +1,16 @@
 package com.lovamimi;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.params.HttpProtocolParams;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.androidhive.jsonparsing.JSONParser;
-
 import android.app.Activity;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+
+import com.androidhive.jsonparsing.JSONParser;
 
 public class MainActivity extends Activity {
 
@@ -39,14 +25,22 @@ public class MainActivity extends Activity {
 				JSONParser parser = new JSONParser();
 				JSONObject obj = parser.getJSONFromUrl("http://lovamimi.com/ja?json=1");
 				Log.d(TAG, "obj=" + obj);
+				try {
+					JSONArray secrets = obj.getJSONArray("secrets");
+					 for(int i = 0; i < secrets.length(); i++){
+						 JSONObject secret = secrets.getJSONObject(i);					
+						 Log.d("", "secret=" + secret.getString("body"));
+					}
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				return null;
 			}
 		}.execute("test");		
 	}
 
 	private static final String TAG = "Lovamimi Client";
-	private static final int DEFAULT_TIMEOUT = 10000;
-	private static final String DEFAULT_USER_AGENT = "Lovamimi For Android/1.0";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
