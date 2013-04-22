6ceed62a65d16ea4708f7653a7ee6a8a548323d3
Taro Minowa Higepon <higepon@gmail.com>
2013-04-21 22:21:49 -0700

    tidy.

--- src/com/lovamimi/CommentActivity.java
+++ src/com/lovamimi/CommentActivity.java
@@ -1,16 +1,11 @@
 package com.lovamimi;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -25,34 +20,24 @@ public class CommentActivity extends Activity {
 		Secret secret = (Secret) intent.getExtras().get("secret");
 		Log.d("debug", secret.toString());
 		ScrollView mainLayout = (ScrollView) findViewById(R.id.scroll);
-		LayoutInflater inflater = (LayoutInflater) getApplicationContext()
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);		
 		RelativeLayout incLayout = (RelativeLayout) mainLayout.findViewById(R.id.secret);
-		TextView tv = (TextView) incLayout
-				.findViewById(R.id.secret_body);
+		TextView tv = (TextView) incLayout.findViewById(R.id.secret_body);
 		tv.setText(secret.body);
-		TextView secretDatetime = (TextView) incLayout
-				.findViewById(R.id.secret_datetime);
+		TextView secretDatetime = (TextView) incLayout.findViewById(R.id.secret_datetime);
 		secretDatetime.setText(secret.datetime);
 
-		
-		TextView numComments = (TextView) incLayout
-				.findViewById(R.id.num_comments);
+		TextView numComments = (TextView) incLayout.findViewById(R.id.num_comments);
 
-		numComments.setText("コメント("
-				+ String.valueOf(secret.numComments) + ")");
-		ImageView commentIcon = (ImageView) incLayout
-				.findViewById(R.id.comment_icon);
-		
+		numComments.setText("コメント(" + String.valueOf(secret.numComments) + ")");
+		ImageView commentIcon = (ImageView) incLayout.findViewById(R.id.comment_icon);
+
 		commentIcon.setImageResource(secret.numComments > 0 ? R.drawable.comment : R.drawable.comment2);
 
-		TextView numLikes = (TextView) incLayout
-				.findViewById(R.id.num_likes);
+		TextView numLikes = (TextView) incLayout.findViewById(R.id.num_likes);
 		numLikes.setText("いいね(" + String.valueOf(secret.numLikes) + ")");
-		
-		ImageView icon = (ImageView) incLayout
-				.findViewById(R.id.profile_image);
-		icon.setImageResource(secret.getIconResource());		
+
+		ImageView icon = (ImageView) incLayout.findViewById(R.id.profile_image);
+		icon.setImageResource(secret.getIconResource());
 	}
 
 	@Override
--- src/com/lovamimi/MainActivity.java
+++ src/com/lovamimi/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends Activity {
 	private void track(String eventName) {
 		mixpanel.track("Android:" + eventName, null);
 	}
-	
+
 	private void getSecrets() {
 		AsyncTask<String, Void, List<Secret>> fetchTimeline = new AsyncTask<String, Void, List<Secret>>() {
 
@@ -55,26 +55,22 @@ public class MainActivity extends Activity {
 
 			private void addSecretsToLayout(List<Secret> secrets) {
 				LinearLayout mainLayout = (LinearLayout) findViewById(R.id.layout_main);
-				LayoutInflater inflater = (LayoutInflater) getApplicationContext()
-						.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(
+						Context.LAYOUT_INFLATER_SERVICE);
 				for (Secret secret : secrets) {
 					addOneSecretToLayout(mainLayout, inflater, secret);
 				}
 			}
 
-			private void addOneSecretToLayout(LinearLayout mainLayout,
-					LayoutInflater inflater, final Secret secret) {
-				RelativeLayout incLayout = (RelativeLayout) inflater.inflate(
-						R.layout.secret, null);
-				TextView tv = (TextView) incLayout
-						.findViewById(R.id.secret_body);
+			private void addOneSecretToLayout(LinearLayout mainLayout, LayoutInflater inflater, final Secret secret) {
+				RelativeLayout incLayout = (RelativeLayout) inflater.inflate(R.layout.secret, null);
+				TextView tv = (TextView) incLayout.findViewById(R.id.secret_body);
 				tv.setText(secret.body);
-				TextView secretDatetime = (TextView) incLayout
-						.findViewById(R.id.secret_datetime);
+				TextView secretDatetime = (TextView) incLayout.findViewById(R.id.secret_datetime);
 				secretDatetime.setText(secret.datetime);
 
 				OnClickListener listener = new OnClickListener() {
-					
+
 					@Override
 					public void onClick(View v) {
 						Intent intent = new Intent(MainActivity.this, CommentActivity.class);
@@ -82,39 +78,32 @@ public class MainActivity extends Activity {
 						startActivity(intent);
 					}
 				};
-				
-				TextView numComments = (TextView) incLayout
-						.findViewById(R.id.num_comments);
+
+				TextView numComments = (TextView) incLayout.findViewById(R.id.num_comments);
 				numComments.setOnClickListener(listener);
-				numComments.setText("コメント("
-						+ String.valueOf(secret.numComments) + ")");
-				ImageView commentIcon = (ImageView) incLayout
-						.findViewById(R.id.comment_icon);
-				
+				numComments.setText("コメント(" + String.valueOf(secret.numComments) + ")");
+				ImageView commentIcon = (ImageView) incLayout.findViewById(R.id.comment_icon);
+
 				commentIcon.setImageResource(secret.numComments > 0 ? R.drawable.comment : R.drawable.comment2);
 				commentIcon.setOnClickListener(listener);
-				TextView numLikes = (TextView) incLayout
-						.findViewById(R.id.num_likes);
+				TextView numLikes = (TextView) incLayout.findViewById(R.id.num_likes);
 				numLikes.setText("いいね(" + String.valueOf(secret.numLikes) + ")");
-				
-				ImageView icon = (ImageView) incLayout
-						.findViewById(R.id.profile_image);
+
+				ImageView icon = (ImageView) incLayout.findViewById(R.id.profile_image);
 				icon.setImageResource(secret.getIconResource());
 				mainLayout.addView(incLayout);
 			}
 
 			@Override
 			protected void onPreExecute() {
-				progressDialog = ProgressDialog.show(MainActivity.this,
-						"Refresh", "Please wait...");
+				progressDialog = ProgressDialog.show(MainActivity.this, "Refresh", "Please wait...");
 				super.onPreExecute();
 			}
 
 			@Override
 			protected List<Secret> doInBackground(String... args) {
 				JSONParser parser = new JSONParser();
-				JSONObject obj = parser
-						.getJSONFromUrl("http://lovamimi.com/ja?json=1");
+				JSONObject obj = parser.getJSONFromUrl("http://lovamimi.com/ja?json=1");
 				Log.d(TAG, "obj=" + obj);
 				ArrayList<Secret> results = new ArrayList<Secret>();
 				try {
@@ -122,12 +111,8 @@ public class MainActivity extends Activity {
 					for (int i = 0; i < secrets.length(); i++) {
 						JSONObject secret = secrets.getJSONObject(i);
 						Log.d("", "secret=" + secret.getString("body"));
-						results.add(new Secret(
-								secret.getString("body"),
-								secret.getString("datetime"),
-								secret.getString("icon"),
-								secret.getInt("num_comments"),
-								secret.getInt("num_likes")));
+						results.add(new Secret(secret.getString("body"), secret.getString("datetime"), secret
+								.getString("icon"), secret.getInt("num_comments"), secret.getInt("num_likes")));
 					}
 					return results;
 				} catch (JSONException e) {
@@ -145,8 +130,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		mixpanel = MixpanelAPI.getInstance(this,
-				"e516b8643dc2d4d9b1779d243b7db7e5");
+		mixpanel = MixpanelAPI.getInstance(this, "e516b8643dc2d4d9b1779d243b7db7e5");
 		setContentView(R.layout.activity_main);
 	}
 
