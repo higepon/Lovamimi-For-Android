e444501eba984195f13a6f954adde0d2a4e176c8
Taro Minowa Higepon <higepon@gmail.com>
2013-05-04 16:00:37 -0700

    renamed.

--- src/com/lovamimi/PostSecretActivity.java
+++ src/com/lovamimi/PostSecretActivity.java
@@ -3,7 +3,7 @@ package com.lovamimi;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class PostSecret extends Activity {
+public class PostSecretActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
