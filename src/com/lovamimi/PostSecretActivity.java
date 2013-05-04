91bf1b4adc91cabc472af929b3ee88ae6d37d9eb
Taro Minowa Higepon <higepon@gmail.com>
2013-05-04 16:27:08 -0700

    Added laytout file for PostSecretActivity.

--- src/com/lovamimi/PostSecretActivity.java
+++ src/com/lovamimi/PostSecretActivity.java
@@ -6,5 +6,6 @@ import android.os.Bundle;
 public class PostSecretActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_secret);
     }
 }
