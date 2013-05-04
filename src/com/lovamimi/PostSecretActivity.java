62a34fec1ecbdf62ec4c9e481c57813a06dadaec
Taro Minowa Higepon <higepon@gmail.com>
2013-05-04 16:40:34 -0700

    Added button and EditText.

--- src/com/lovamimi/PostSecretActivity.java
+++ src/com/lovamimi/PostSecretActivity.java
@@ -2,10 +2,22 @@ package com.lovamimi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PostSecretActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_secret);
+        Button postButton = (Button) findViewById(R.id.post_button);
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText secretText = (EditText) findViewById(R.id.secret_text);
+                Log.d("hage", secretText.getText().toString());
+            }
+        });
     }
 }
