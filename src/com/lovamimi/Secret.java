0c80e79108f7e6a7ccfb71051fbc00a7137bcb6e
Taro Minowa Higepon <higepon@gmail.com>
2013-05-05 13:57:31 -0700

    tidy.

--- src/com/lovamimi/Secret.java
+++ src/com/lovamimi/Secret.java
@@ -1,12 +1,7 @@
 package com.lovamimi;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import android.webkit.CookieManager;
-import android.webkit.CookieSyncManager;
+import android.util.Log;
+import com.androidhive.jsonparsing.JSONParser;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -24,77 +19,78 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.util.Log;
-
-import com.androidhive.jsonparsing.JSONParser;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class Secret implements Serializable {
-	private static final long serialVersionUID = 0x12345678L;
+    private static final long serialVersionUID = 0x12345678L;
 
-	String body;
-	String datetime;
-	String iconName;
-	int numComments;
-	int numLikes;
-	List<Secret> comments;
+    String body;
+    String datetime;
+    String iconName;
+    int numComments;
+    int numLikes;
+    List<Secret> comments;
 
-	@Override
-	public String toString() {
-		return "Secret [body=" + body + ", datetime=" + datetime + ", iconName=" + iconName + ", numComments="
-				+ numComments + ", numLikes=" + numLikes + "]";
-	}
+    @Override
+    public String toString() {
+        return "Secret [body=" + body + ", datetime=" + datetime + ", iconName=" + iconName + ", numComments="
+                + numComments + ", numLikes=" + numLikes + "]";
+    }
 
-	public Secret(String body, String datetime, String iconName, int numComments, int numLikes) {
-		super();
-		this.body = body;
-		this.datetime = datetime;
-		this.iconName = iconName;
-		this.numComments = numComments;
-		this.numLikes = numLikes;
-	}
+    public Secret(String body, String datetime, String iconName, int numComments, int numLikes) {
+        super();
+        this.body = body;
+        this.datetime = datetime;
+        this.iconName = iconName;
+        this.numComments = numComments;
+        this.numLikes = numLikes;
+    }
 
-	public int getIconResource() {
-		if (iconName.equals("0.jpg")) {
-			return R.drawable.lovamimi_0;
-		} else if (iconName.equals("1.jpg")) {
-			return R.drawable.lovamimi_1;
-		} else if (iconName.equals("2.jpg")) {
-			return R.drawable.lovamimi_2;
-		} else if (iconName.equals("2.jpg")) {
-			return R.drawable.lovamimi_2;
-		} else if (iconName.equals("3.jpg")) {
-			return R.drawable.lovamimi_3;
-		} else if (iconName.equals("4.jpg")) {
-			return R.drawable.lovamimi_4;
-		} else if (iconName.equals("5.jpg")) {
-			return R.drawable.lovamimi_5;
-		} else if (iconName.equals("6.jpg")) {
-			return R.drawable.lovamimi_6;
-		} else if (iconName.equals("7.jpg")) {
-			return R.drawable.lovamimi_7;
-		} else if (iconName.equals("8.jpg")) {
-			return R.drawable.lovamimi_8;
-		} else if (iconName.equals("9.jpg")) {
-			return R.drawable.lovamimi_9;
-		} else {
-			return R.drawable.lovamimi_0;
-		}
-	}
-	
-	public static List<Secret> getSecrets() {
-		JSONParser parser = new JSONParser();
-		JSONObject obj = parser.getJSONFromUrl("http://lovamimi.com/ja?json=1");
-		ArrayList<Secret> results = new ArrayList<Secret>();
-		try {
-			JSONArray secrets = obj.getJSONArray("secrets");
-			Secret.extractSecrets(results, secrets);
-			return results;
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
+    public int getIconResource() {
+        if (iconName.equals("0.jpg")) {
+            return R.drawable.lovamimi_0;
+        } else if (iconName.equals("1.jpg")) {
+            return R.drawable.lovamimi_1;
+        } else if (iconName.equals("2.jpg")) {
+            return R.drawable.lovamimi_2;
+        } else if (iconName.equals("2.jpg")) {
+            return R.drawable.lovamimi_2;
+        } else if (iconName.equals("3.jpg")) {
+            return R.drawable.lovamimi_3;
+        } else if (iconName.equals("4.jpg")) {
+            return R.drawable.lovamimi_4;
+        } else if (iconName.equals("5.jpg")) {
+            return R.drawable.lovamimi_5;
+        } else if (iconName.equals("6.jpg")) {
+            return R.drawable.lovamimi_6;
+        } else if (iconName.equals("7.jpg")) {
+            return R.drawable.lovamimi_7;
+        } else if (iconName.equals("8.jpg")) {
+            return R.drawable.lovamimi_8;
+        } else if (iconName.equals("9.jpg")) {
+            return R.drawable.lovamimi_9;
+        } else {
+            return R.drawable.lovamimi_0;
+        }
+    }
+
+    public static List<Secret> getSecrets() {
+        JSONParser parser = new JSONParser();
+        JSONObject obj = parser.getJSONFromUrl("http://lovamimi.com/ja?json=1");
+        ArrayList<Secret> results = new ArrayList<Secret>();
+        try {
+            JSONArray secrets = obj.getJSONArray("secrets");
+            Secret.extractSecrets(results, secrets);
+            return results;
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
 
     // todo move
     public static String chop(String s) {
@@ -109,6 +105,7 @@ public class Secret implements Serializable {
         }
     }
 
+    // todo: Extract http post
     public static boolean post(String sessionId, String text) {
         // Server side bug, sessionId has "\n" on it's tail.
         sessionId = Secret.chop(sessionId);
@@ -125,7 +122,7 @@ public class Secret implements Serializable {
         cookieStore.addCookie(cookie);
         httpContext.setAttribute(ClientContext.COOKIE_STORE, cookieStore);
         List<NameValuePair> params = new ArrayList<NameValuePair>();
-        params.add(new BasicNameValuePair("body", text));
+        params.add(new BasicNameValuePair("text", text));
         params.add(new BasicNameValuePair("lang", "ja"));
         try {
             httpPost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
@@ -163,51 +160,52 @@ public class Secret implements Serializable {
         }
         return sb.toString();
     }
-/*
-    NSMutableURLRequest* request = [Secret createLovamimiRequest:@"http://lovamimi.com/ja" requestBody:requestBody];
-    SecretPoster* poster = [[SecretPoster alloc] initWithDelegate:delegate];
-    NSURLConnection* conn = [[NSURLConnection alloc] initWithRequest:request delegate:poster];
-    if (conn == nil) {
-        [delegate onSecretPostComplete:@"接続エラー"];
-        return;
+
+    /*
+        NSMutableURLRequest* request = [Secret createLovamimiRequest:@"http://lovamimi.com/ja" requestBody:requestBody];
+        SecretPoster* poster = [[SecretPoster alloc] initWithDelegate:delegate];
+        NSURLConnection* conn = [[NSURLConnection alloc] initWithRequest:request delegate:poster];
+        if (conn == nil) {
+            [delegate onSecretPostComplete:@"接続エラー"];
+            return;
+        }
+        NSMutableURLRequest* request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:url]];
+        if (requestBody != nil) {
+            request.HTTPMethod = @"POST";
+            request.HTTPBody = [requestBody dataUsingEncoding:NSUTF8StringEncoding];
+        } else {
+            request.HTTPMethod = @"GET";
+        }
+        AppDelegate* appDelegate = (AppDelegate*)[[UIApplication sharedApplication] delegate];
+        assert(appDelegate);
+        NSDictionary *properties = [NSDictionary dictionaryWithObjectsAndKeys:
+                                  @"lovamimi.com", NSHTTPCookieDomain,
+                                  @"/", NSHTTPCookiePath,
+                                  @"SESSION_ID", NSHTTPCookieName,
+                                  appDelegate.session.id, NSHTTPCookieValue,
+                                  nil];
+        NSHTTPCookie *cookie = [NSHTTPCookie cookieWithProperties:properties];
+        NSArray* cookies = [NSArray arrayWithObjects: cookie, nil];
+        NSDictionary * headers = [NSHTTPCookie requestHeaderFieldsWithCookies:cookies];
+        [request setAllHTTPHeaderFields:headers];
+        return request;
+         */
+    private static void extractSecrets(ArrayList<Secret> results, JSONArray secrets) throws JSONException {
+        for (int i = 0; i < secrets.length(); i++) {
+            JSONObject secret = secrets.getJSONObject(i);
+            JSONArray commentsArray = secret.has("comments") ? secret.getJSONArray("comments") : null;
+            ArrayList<Secret> comments;
+            if (commentsArray == null) {
+                comments = new ArrayList<Secret>(0);
+            } else {
+                comments = new ArrayList<Secret>(commentsArray.length());
+                extractSecrets(comments, commentsArray);
+            }
+            Log.d("", "secret=" + secret.getString("body"));
+            Secret s = new Secret(secret.getString("body"), secret.getString("datetime"), secret.getString("icon"),
+                    secret.getInt("num_comments"), secret.getInt("num_likes"));
+            s.comments = comments;
+            results.add(s);
+        }
     }
-    NSMutableURLRequest* request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:url]];
-    if (requestBody != nil) {
-        request.HTTPMethod = @"POST";
-        request.HTTPBody = [requestBody dataUsingEncoding:NSUTF8StringEncoding];
-    } else {
-        request.HTTPMethod = @"GET";
-    }
-    AppDelegate* appDelegate = (AppDelegate*)[[UIApplication sharedApplication] delegate];
-    assert(appDelegate);
-    NSDictionary *properties = [NSDictionary dictionaryWithObjectsAndKeys:
-                              @"lovamimi.com", NSHTTPCookieDomain,
-                              @"/", NSHTTPCookiePath,
-                              @"SESSION_ID", NSHTTPCookieName,
-                              appDelegate.session.id, NSHTTPCookieValue,
-                              nil];
-    NSHTTPCookie *cookie = [NSHTTPCookie cookieWithProperties:properties];
-    NSArray* cookies = [NSArray arrayWithObjects: cookie, nil];
-    NSDictionary * headers = [NSHTTPCookie requestHeaderFieldsWithCookies:cookies];
-    [request setAllHTTPHeaderFields:headers];
-    return request;
-     */
-	private static void extractSecrets(ArrayList<Secret> results, JSONArray secrets) throws JSONException {
-		for (int i = 0; i < secrets.length(); i++) {
-			JSONObject secret = secrets.getJSONObject(i);
-			JSONArray commentsArray = secret.has("comments") ? secret.getJSONArray("comments") : null;
-			ArrayList<Secret> comments;
-			if (commentsArray == null) {
-				comments = new ArrayList<Secret>(0);
-			} else {
-				comments = new ArrayList<Secret>(commentsArray.length());
-				extractSecrets(comments, commentsArray);
-			}
-			Log.d("", "secret=" + secret.getString("body"));
-			Secret s = new Secret(secret.getString("body"), secret.getString("datetime"), secret.getString("icon"),
-					secret.getInt("num_comments"), secret.getInt("num_likes"));
-			s.comments = comments;
-			results.add(s);
-		}
-	}
 }
